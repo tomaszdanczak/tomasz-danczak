36de4b4a0963877e51d08391a336991ec507a1e3
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-12-09 11:28:37 +0100

    feat: create and use <HeroLogo/> component

--- src/components/organisms/Navigation/Navigation.jsx
+++ src/components/organisms/Navigation/Navigation.jsx
@@ -3,6 +3,7 @@ import { Fragment } from "react"
 import { Popover, Transition } from "@headlessui/react"
 import { MenuIcon, XIcon } from "@heroicons/react/outline"
 import { navigation } from "data/navigationItemsData"
+import HeroLogo from "components/molecules/HeroLogo/HeroLogo"
 
 export default function Navigation() {
   return (
@@ -14,13 +15,14 @@ export default function Navigation() {
         >
           <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
             <div className="flex items-center justify-between w-full md:w-auto">
-              <a href="#">
+              <HeroLogo />
+              {/* <a href="#">
                 <span className="sr-only">Workflow</span>
                 <img
                   className="h-8 w-auto sm:h-10"
                   src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                 />
-              </a>
+              </a> */}
               <div className="-mr-2 flex items-center md:hidden">
                 <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                   <span className="sr-only">Open main menu</span>
