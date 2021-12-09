62c78d1208d7eb19a21fb583a5c77625e6d24ff8
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-12-09 14:06:39 +0100

    feat: use GraphQl to get content data from DatoCMS (use data in the project)

--- src/components/organisms/SocialSection/SocialSection.jsx
+++ src/components/organisms/SocialSection/SocialSection.jsx
@@ -1,16 +1,30 @@
 import React from "react"
-import SocialLink from "../../atoms/SocialLink/SocialLink"
-import SocialHeader from "../../molecules/SocialHeader/SocialHeader"
+import SocialLink from "components/atoms/SocialLink/SocialLink"
+import SocialHeader from "components/molecules/SocialHeader/SocialHeader"
+import { graphql, useStaticQuery } from "gatsby"
 
 export default function SocialSection() {
+  const data = useStaticQuery(graphql`
+    {
+      allDatoCmsHeading(filter: { section: { eq: "SocialSection" } }) {
+        nodes {
+          subtitle
+          title
+          title2
+        }
+      }
+    }
+  `)
+
+  const [headingData] = data.allDatoCmsHeading.nodes
+
   return (
     <div className="bg-indigo-700">
       <div className="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
         <SocialHeader
-          span1="Boost your productivity."
-          span2="Start using Workflow today."
-          text="Ac euismod vel sit maecenas id pellentesque eu sed consectetur.
-          Malesuada adipiscing sagittis vel nulla nec."
+          span1={headingData.title}
+          span2={headingData.title2}
+          text={headingData.subtitle}
         />
 
         <div className="sm:space-x-8">
