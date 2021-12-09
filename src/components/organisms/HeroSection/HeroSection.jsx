fac77ffcd3485b220e66b982f472ed81eb6f5dc0
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-12-09 11:07:08 +0100

    feat: create and use <Polygon/> component

--- src/components/organisms/HeroSection/HeroSection.jsx
+++ src/components/organisms/HeroSection/HeroSection.jsx
@@ -3,21 +3,14 @@ import { Fragment } from "react"
 import { Popover, Transition } from "@headlessui/react"
 import { MenuIcon, XIcon } from "@heroicons/react/outline"
 import { navigation } from "data/navigationItemsData"
+import Polygon from "components/atoms/Polygon/Polygon"
 
 export default function HeroSection() {
   return (
     <div className="relative bg-white overflow-hidden">
       <div className="max-w-7xl mx-auto">
         <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-          <svg
-            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
-            fill="currentColor"
-            viewBox="0 0 100 100"
-            preserveAspectRatio="none"
-            aria-hidden="true"
-          >
-            <polygon points="50,0 100,0 50,100 0,100" />
-          </svg>
+          <Polygon />
 
           <Popover>
             <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
