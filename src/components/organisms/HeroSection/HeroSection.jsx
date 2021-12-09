6bff8251d635ac360b4a8604ca87fd4742fd1a81
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-12-09 10:46:14 +0100

    feat: extract navigation items to the other file

--- src/components/organisms/HeroSection/HeroSection.jsx
+++ src/components/organisms/HeroSection/HeroSection.jsx
@@ -2,13 +2,7 @@ import React from "react"
 import { Fragment } from "react"
 import { Popover, Transition } from "@headlessui/react"
 import { MenuIcon, XIcon } from "@heroicons/react/outline"
-
-const navigation = [
-  { name: "Product", href: "#" },
-  { name: "Features", href: "#" },
-  { name: "Marketplace", href: "#" },
-  { name: "Company", href: "#" },
-]
+import { navigation } from "./../../../data/navigationItemsData"
 
 export default function HeroSection() {
   return (
---
+++ src/data/navigationItemsData.js
@@ -0,0 +1,7 @@
+export const navigation = [
+  { name: "About", href: "#" },
+  { name: "Blog", href: "#" },
+  { name: "Stack", href: "#" },
+  { name: "Projects", href: "#" },
+  { name: "Contact", href: "#" },
+]
