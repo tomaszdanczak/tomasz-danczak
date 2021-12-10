0de04fd9cd7e5671de77e41ae800e64cb1b4ec14
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-12-10 10:01:36 +0100

    feat: create <Article/> component using boilerplate code and use it

--- src/components/templates/PostLayout/PostLayout.jsx
+++ src/components/templates/PostLayout/PostLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import HeroSection from "components/organisms/HeroSection/HeroSection"
+import Article from "components/organisms/Article/Article"
 
 export default function PostLayout() {
   return (
     <>
       <HeroSection />
+      <Article />
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require("@tailwindcss/aspect-ratio")
+  ],
 }
