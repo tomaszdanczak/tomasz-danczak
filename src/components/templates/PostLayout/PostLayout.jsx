d9b096d25cc2b9a4cf959f22cce62875349ea16c
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-12-10 09:40:03 +0100

    feat: create <PostLayout/> component

---
+++ src/components/templates/PostLayout/PostLayout.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import HeroSection from "components/organisms/HeroSection/HeroSection"
+
+export default function PostLayout() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  )
+}
