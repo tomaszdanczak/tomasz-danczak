3398435d954c9f23d862c9c98f57aca16d50801c
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-12-10 08:28:31 +0100

    feat: add boilerplate code

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import * as React from "react"
 import HeroSection from "components/organisms/HeroSection/HeroSection"
 import AboutSection from "components/organisms/AboutSection/AboutSection"
 import SocialSection from "components/organisms/SocialSection/SocialSection"
+import BlogSection from "components/organisms/BlogSection/BlogSection"
 
 const IndexPage = () => (
   <>
     <HeroSection />
     <AboutSection />
     <SocialSection />
+    <BlogSection />
   </>
 )
 
