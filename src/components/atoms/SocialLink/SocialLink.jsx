29b2c22a0ab5e0ea1784fcbb4c3cf7b58c6f3972
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-12-10 09:25:35 +0100

    fix: use <a> HTML tag instead Gatsby <Link/> component

--- src/components/atoms/SocialLink/SocialLink.jsx
+++ src/components/atoms/SocialLink/SocialLink.jsx
@@ -1,13 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
 
 export default function SocialLink({ label, href }) {
   return (
-    <Link
-      to={href}
+    <a
+      href={href}
       className="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50 sm:w-auto"
     >
       {label}
-    </Link>
+    </a>
   )
 }
