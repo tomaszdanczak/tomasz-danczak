7a85efd1ff2978bb101aa274d666de6cd9a00973
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-12-10 08:56:18 +0100

    feat: use GraphQL to get content data from DatoCMS (use data in the project)

--- src/components/organisms/BlogSection/BlogSection.jsx
+++ src/components/organisms/BlogSection/BlogSection.jsx
@@ -1,65 +1,47 @@
 import React from "react"
 import Header from "components/molecules/Header/Header"
-import BlogItem from "../../molecules/BlogItem/BlogItem"
-
-const posts = [
-  {
-    title: "Boost your conversion rate",
-    href: "#",
-    category: { name: "Article", href: "#" },
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.",
-    date: "Mar 16, 2020",
-    datetime: "2020-03-16",
-    imageUrl:
-      "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
-    readingTime: "6 min",
-    author: {
-      name: "Roel Aufderehar",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-  },
-  {
-    title: "How to use search engine optimization to drive sales",
-    href: "#",
-    category: { name: "Video", href: "#" },
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.",
-    date: "Mar 10, 2020",
-    datetime: "2020-03-10",
-    imageUrl:
-      "https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
-    readingTime: "4 min",
-    author: {
-      name: "Brenna Goyette",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-  },
-  {
-    title: "Improve your customer experience",
-    href: "#",
-    category: { name: "Case Study", href: "#" },
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.",
-    date: "Feb 12, 2020",
-    datetime: "2020-02-12",
-    imageUrl:
-      "https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
-    readingTime: "11 min",
-    author: {
-      name: "Daniela Metz",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-  },
-]
+import BlogItem from "components/molecules/BlogItem/BlogItem"
+import { graphql, useStaticQuery } from "gatsby"
 
 export default function BlogSection() {
+  const data = useStaticQuery(graphql`
+    {
+      allDatoCmsBlogPost {
+        nodes {
+          title
+          description
+          author
+          category
+          authorimage {
+            fluid {
+              sizes
+              src
+              srcSet
+            }
+          }
+          datetime(formatString: "")
+          readingtime
+          image {
+            fluid {
+              sizes
+              src
+              srcSet
+            }
+          }
+        }
+      }
+      allDatoCmsHeading(filter: { section: { eq: "BlogSection" } }) {
+        nodes {
+          subtitle
+          title
+          special
+        }
+      }
+    }
+  `)
+  const posts = data.allDatoCmsBlogPost.nodes
+  const [headingData] = data.allDatoCmsHeading.nodes
+
   return (
     <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
       <div className="absolute inset-0">
@@ -68,9 +50,8 @@ export default function BlogSection() {
       <div className="relative max-w-7xl mx-auto">
         <Header
           centreFrom="sm"
-          title="From the blog"
-          subtitle="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa
-          libero labore natus atque, ducimus sed."
+          title={headingData.title}
+          subtitle={headingData.subtitle}
         />
 
         <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
