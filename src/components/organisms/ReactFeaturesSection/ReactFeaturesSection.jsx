59188c3fcad179c5d9fa7197f56df8561a658730
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-12-13 08:54:52 +0100

    ref: use <Header/> component in the <ReactFeaturesSection/>

--- src/components/organisms/ReactFeaturesSection/ReactFeaturesSection.jsx
+++ src/components/organisms/ReactFeaturesSection/ReactFeaturesSection.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Header from "components/molecules/Header/Header"
 
 import { CheckIcon } from "@heroicons/react/outline"
 
@@ -49,18 +50,13 @@ export default function ReactFeaturesSection() {
   return (
     <div className="bg-white">
       <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
-        <div>
-          <h2 className="text-base font-semibold text-indigo-600 uppercase tracking-wide">
-            Everything you need
-          </h2>
-          <p className="mt-2 text-3xl font-extrabold text-gray-900">
-            All-in-one platform
-          </p>
-          <p className="mt-4 text-lg text-gray-500">
-            Ac euismod vel sit maecenas id pellentesque eu sed consectetur.
-            Malesuada adipiscing sagittis vel nulla nec.
-          </p>
-        </div>
+        <Header
+          decoration="Everything you need"
+          title="All-in-one platform"
+          subtitle="Ac euismod vel sit maecenas id pellentesque eu sed consectetur.
+          Malesuada adipiscing sagittis vel nulla nec."
+        />
+
         <div className="mt-12 lg:mt-0 lg:col-span-2">
           <dl className="space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:grid-rows-4 sm:grid-flow-col sm:gap-x-6 sm:gap-y-10 lg:gap-x-8">
             {features.map(feature => (
