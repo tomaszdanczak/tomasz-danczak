54f99e86204cab3e7c96bcba1adf1c6d404c83b8
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-12-13 09:01:01 +0100

    feat: use GrapgQL to get content data from DatoCMS (use data in the project)

--- src/components/organisms/ReactFeaturesSection/ReactFeaturesSection.jsx
+++ src/components/organisms/ReactFeaturesSection/ReactFeaturesSection.jsx
@@ -1,59 +1,37 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 import Header from "components/molecules/Header/Header"
 import TechnologyFeature from "components/molecules/TechnologyFeature/TechnologyFeature"
 
-const features = [
-  {
-    name: "Invite team members",
-    description:
-      "You can manage phone, email and chat conversations all from a single mailbox.",
-  },
-  {
-    name: "List view",
-    description:
-      "You can manage phone, email and chat conversations all from a single mailbox.",
-  },
-  {
-    name: "Keyboard shortcuts",
-    description:
-      "You can manage phone, email and chat conversations all from a single mailbox.",
-  },
-  {
-    name: "Calendars",
-    description:
-      "You can manage phone, email and chat conversations all from a single mailbox.",
-  },
-  {
-    name: "Notifications",
-    description:
-      "Find what you need with advanced filters, bulk actions, and quick views.",
-  },
-  {
-    name: "Boards",
-    description:
-      "Find what you need with advanced filters, bulk actions, and quick views.",
-  },
-  {
-    name: "Reporting",
-    description:
-      "Find what you need with advanced filters, bulk actions, and quick views.",
-  },
-  {
-    name: "Mobile app",
-    description:
-      "Find what you need with advanced filters, bulk actions, and quick views.",
-  },
-]
-
 export default function ReactFeaturesSection() {
+  const data = useStaticQuery(graphql`
+    {
+      allDatoCmsReactfeature {
+        nodes {
+          title
+          description
+        }
+      }
+      allDatoCmsHeading(filter: { section: { eq: "ReactFeaturesSection" } }) {
+        nodes {
+          subtitle
+          title
+          special
+        }
+      }
+    }
+  `)
+
+  const features = data.allDatoCmsReactfeature.nodes
+  const [headingData] = data.allDatoCmsHeading.nodes
+
   return (
     <div className="bg-white">
       <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
         <Header
-          decoration="Everything you need"
-          title="All-in-one platform"
-          subtitle="Ac euismod vel sit maecenas id pellentesque eu sed consectetur.
-          Malesuada adipiscing sagittis vel nulla nec."
+          decoration={headingData.special}
+          title={headingData.title}
+          subtitle={headingData.subtitle}
         />
 
         <div className="mt-12 lg:mt-0 lg:col-span-2">
