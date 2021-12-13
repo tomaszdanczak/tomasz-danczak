9738c78ecbf95989bfe896fab31ddcd8225c87e0
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-12-13 10:49:11 +0100

    feat: use GraphQL to get content data from DatoCMS (use data in the project)

--- src/components/molecules/Contact/Contact.jsx
+++ src/components/molecules/Contact/Contact.jsx
@@ -1,20 +1,36 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 import { MailIcon, PhoneIcon } from "@heroicons/react/outline"
 import ContactData from "components/molecules/ContactData/ContactData"
 
 export default function Contact() {
+  const data = useStaticQuery(graphql`
+    {
+      allDatoCmsContact {
+        nodes {
+          address
+          address2
+          phone
+          email
+        }
+      }
+    }
+  `)
+
+  const [contactData] = data.allDatoCmsContact.nodes
+
   return (
     <dl className="mt-8 text-base text-gray-500">
       <ContactData
         srOnly="Postal address"
-        paragraph1="742 Evergreen Terrace"
-        paragraph2="Springfield, OR 12345"
+        paragraph1={contactData.address}
+        paragraph2={contactData.address2}
       />
 
       <ContactData
         srOnly="Phone number"
         marginTop="mt-6"
-        span="+1 (555) 123-4567"
+        span={contactData.phone}
         icon={
           <PhoneIcon
             className="flex-shrink-0 h-6 w-6 text-gray-400"
@@ -26,7 +42,7 @@ export default function Contact() {
       <ContactData
         srOnly="Email"
         marginTop="mt-3"
-        span="support@example.com"
+        span={contactData.email}
         icon={
           <MailIcon
             className="flex-shrink-0 h-6 w-6 text-gray-400"
