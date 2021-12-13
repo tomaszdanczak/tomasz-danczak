9fffaf6eb73fbaa39b9c0120bd95e861e0ecce3f
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-12-13 10:29:55 +0100

    ref: use <Heading/> component in the <ContactSection/>

--- src/components/organisms/ContactSection/ContactSection.jsx
+++ src/components/organisms/ContactSection/ContactSection.jsx
@@ -1,4 +1,6 @@
 import React from "react"
+import Heading from "components/molecules/Heading/Heading"
+
 import { MailIcon, PhoneIcon } from "@heroicons/react/outline"
 
 export default function ContactSection() {
@@ -9,14 +11,13 @@ export default function ContactSection() {
       </div>
       <div className="relative max-w-7xl mx-auto lg:grid lg:grid-cols-5">
         <div className="bg-gray-50 py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12">
-          <div className="max-w-lg mx-auto">
-            <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
-              Get in touch
-            </h2>
-            <p className="mt-3 text-lg leading-6 text-gray-500">
-              Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat
-              massa dictumst amet. Sapien tortor lacus arcu.
-            </p>
+          <div className="max-w-lg mx-auto space-y-5 sm:space-y-4">
+            <Heading
+              title="Get in touch"
+              subtitle="Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat
+              massa dictumst amet. Sapien tortor lacus arcu."
+            />
+
             <dl className="mt-8 text-base text-gray-500">
               <div>
                 <dt className="sr-only">Postal address</dt>
