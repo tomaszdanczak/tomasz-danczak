574f6c4d6d8f22f48114be2dbc7fced235cb2dde
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-12-13 10:01:20 +0100

    ref: use <Header/> component in <OtherProjectsSection/>

--- src/components/organisms/OtherProjectsSection/OtherProjectsSection.jsx
+++ src/components/organisms/OtherProjectsSection/OtherProjectsSection.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Header from "components/molecules/Header/Header"
 
 const people = [
   {
@@ -17,15 +18,12 @@ export default function OtherProjectsSection() {
     <div className="bg-white">
       <div className="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
         <div className="space-y-12">
-          <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
-            <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">
-              Our Team
-            </h2>
-            <p className="text-xl text-gray-500">
-              Odio nisi, lectus dis nulla. Ultrices maecenas vitae rutrum dolor
-              ultricies donec risus sodales. Tempus quis et.
-            </p>
-          </div>
+          <Header
+            title="Our Team"
+            subtitle="Odio nisi, lectus dis nulla. Ultrices maecenas vitae rutrum dolor
+              ultricies donec risus sodales. Tempus quis et."
+          />
+
           <ul
             role="list"
             className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8"
