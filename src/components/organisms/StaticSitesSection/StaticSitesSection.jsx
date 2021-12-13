c5d36807ae37ff461bb3ed97b0cd88a4ce3b4623
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-12-13 08:34:40 +0100

    ref: use <Header/> component in the <StaticSitesSection/> component

--- src/components/organisms/StaticSitesSection/StaticSitesSection.jsx
+++ src/components/organisms/StaticSitesSection/StaticSitesSection.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Header from "components/molecules/Header/Header"
 import { CheckIcon } from "@heroicons/react/outline"
 
 const features = [
@@ -48,15 +49,13 @@ export default function StaticSitesSection() {
   return (
     <div className="bg-white">
       <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8">
-        <div className="max-w-3xl mx-auto text-center">
-          <h2 className="text-3xl font-extrabold text-gray-900">
-            All-in-one platform
-          </h2>
-          <p className="mt-4 text-lg text-gray-500">
-            Ac euismod vel sit maecenas id pellentesque eu sed consectetur.
-            Malesuada adipiscing sagittis vel nulla nec.
-          </p>
-        </div>
+        <Header
+          centreFrom="sm"
+          title="All-in-one platform"
+          subtitle="Ac euismod vel sit maecenas id pellentesque eu sed consectetur.
+          Malesuada adipiscing sagittis vel nulla nec."
+        />
+
         <dl className="mt-12 space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 lg:grid-cols-4 lg:gap-x-8">
           {features.map(feature => (
             <div key={feature.name} className="relative">
