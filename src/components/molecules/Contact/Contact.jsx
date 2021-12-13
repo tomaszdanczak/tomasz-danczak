03d72f0ba1c838deaed7481afb423b172fe18b9f
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-12-13 10:42:39 +0100

    ref: create and use <ContactData/> component

--- src/components/molecules/Contact/Contact.jsx
+++ src/components/molecules/Contact/Contact.jsx
@@ -1,36 +1,39 @@
 import React from "react"
 import { MailIcon, PhoneIcon } from "@heroicons/react/outline"
+import ContactData from "components/molecules/ContactData/ContactData"
 
 export default function Contact() {
   return (
     <dl className="mt-8 text-base text-gray-500">
-      <div>
-        <dt className="sr-only">Postal address</dt>
-        <dd>
-          <p>742 Evergreen Terrace</p>
-          <p>Springfield, OR 12345</p>
-        </dd>
-      </div>
-      <div className="mt-6">
-        <dt className="sr-only">Phone number</dt>
-        <dd className="flex">
+      <ContactData
+        srOnly="Postal address"
+        paragraph1="742 Evergreen Terrace"
+        paragraph2="Springfield, OR 12345"
+      />
+
+      <ContactData
+        srOnly="Phone number"
+        marginTop="mt-6"
+        span="+1 (555) 123-4567"
+        icon={
           <PhoneIcon
             className="flex-shrink-0 h-6 w-6 text-gray-400"
             aria-hidden="true"
           />
-          <span className="ml-3">+1 (555) 123-4567</span>
-        </dd>
-      </div>
-      <div className="mt-3">
-        <dt className="sr-only">Email</dt>
-        <dd className="flex">
+        }
+      />
+
+      <ContactData
+        srOnly="Email"
+        marginTop="mt-3"
+        span="support@example.com"
+        icon={
           <MailIcon
             className="flex-shrink-0 h-6 w-6 text-gray-400"
             aria-hidden="true"
           />
-          <span className="ml-3">support@example.com</span>
-        </dd>
-      </div>
+        }
+      />
     </dl>
   )
 }
