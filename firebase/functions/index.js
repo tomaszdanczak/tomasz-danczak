d740febce17f9e50709966a4fe67bfe89c0786ea
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-12-15 11:17:30 +0100

    feat: create sendEmail function and use it in the project

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,9 +1,29 @@
 const functions = require("firebase-functions");
+const cors = require("cors")({ origin: true })
+const sgMail = require('@sendgrid/mail')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.sendEmail = functions.https.onRequest((req, res) => {
+
+  const msg = {
+    to: "9452891@gmail.com",
+    from: "tomaszdanczak@gmail.com",
+    subject: `New message from ${req.body.name} (${req.body.email})`,
+    text: `
+    Message from: ${req.body.name}
+    Phone: ${req.body.phone}
+    Email: ${req.body.email}
+    Message: ${req.body.message}
+    `,
+  }
+
+  cors(req, res, () => {
+    sgMail.setApiKey(functions.config().sendgrid.key)
+
+    sgMail.send(msg).then(() => {
+      console.log('Email sent')
+    })
+      .catch((error) => {
+        console.error(error.response.body)
+      })
+  })
+});
